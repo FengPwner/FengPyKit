d5c6e2e62434539c5ce47c4a1ef953812642b551
刘宝峰 <lbfliubaofeng@gmail.com>
2026-09-21 15:55:29 +0800

    Update version number to S1.2

--- FengDDoS.py
+++ FengDDoS.py
@@ -67,7 +67,7 @@ def show_banner():
     print(f"{color.BOLD} Github :{color.RESET} https://github.com/FengPwner")
     print(f"{color.BOLD} Atomgit:{color.RESET} https://atomgit.com/FengPwner")
     print(f"{color.BOLD} CSDN   :{color.RESET} https://blog.csdn.net/2302_76189356")
-    print(f"{color.BOLD} Version:{color.RESET} 0.4.2")
+    print(f"{color.BOLD} Version:{color.RESET} S1.2")
     print(f"{color.YELLOW}---------------------------------------------------{color.RESET}")
     print(f"{color.RED}{color.BOLD} [!] Do not use for illegal purposes!{color.RESET}\n")
 
